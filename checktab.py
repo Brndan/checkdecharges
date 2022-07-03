33888479f6277b4bf363dfa5a7ef28666bd99bb4
Brendan Chabannes <brendan.chabannes@hey.com>
2022-07-03 11:10:45 +0200

    add input path

--- checktab.py
+++ checktab.py
@@ -1,6 +1,22 @@
 #/usr/bin/env python3
-
+import sys
 import csv
+from pathlib import Path
+import argparse
+
+parser = argparse.ArgumentParser(description='Chemin où trouver le tableau.')
+parser.add_argument('input',
+                    help='Entrez la source au format CSV.')
+
+args = parser.parse_args()
+
+if args.input:
+    source_folder = Path(args.input)
+    print("Je cherche dans : " + args.input)
+else:
+    print("Impossible de trouver la source")
+
+sys.exit()
 
 with open('perm.csv',newline='') as csvfile:
     fichier = csv.reader(csvfile,delimiter=',',quotechar='"')
